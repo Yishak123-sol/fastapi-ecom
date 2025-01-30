0b19e1f52a25316ceea32c8f24c59c6aa2692daa
Yishak <yisehaksolomon19@gmail.com>
2025-01-30 16:48:53 +0300

    product add

--- app/models.py
+++ app/models.py
@@ -19,7 +19,6 @@ class User(Base):
 
 class Product(Base):
     __tablename__ = "products"
-
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     description = Column(String, nullable=False)
--- app/router/product.py
+++ app/router/product.py
@@ -9,33 +9,37 @@ from .. import oauth2
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
-# @router.get("/")
-# def read_products(
-#     db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
-# ):
-#     products = db.query(models.Product).all()
-#     if not products:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND, detail="No products found"
-#         )
-#     return products.__dict__
+@router.get("/")
+def read_products(
+    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
+):
+    products = db.query(models.Product).all()
+    if not products:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No products found"
+        )
+    return products.__dict__
 
 
-# @router.get("/{id}")
-# def read_products_by_id(
-#     db: Session = Depends(get_db),
-#     current_user: int = Depends(oauth2.get_current_user),
-# ):
-#     products = db.query(models.Product).filter(models.Product.id == id).first()
-#     if not products:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND, detail="No products found"
-#         )
+@router.get("/{id}")
+def read_products_by_id(
+    id: int,
+    db: Session = Depends(get_db),
+    # current_user: int = Depends(oauth2.get_current_user),
+):
+    products = db.query(models.Product).filter(models.Product.owner_id == id).all()
+    if not products:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No products found"
+        )
+    return products
 
-#     return products.__dict__
 
-
-@router.post("/", response_model=schema.ProductResponse)
+@router.post(
+    "/",
+    response_model=schema.ProductResponse,
+    # current_user: int = Depends(oauth2.get_current_user),
+)
 def create_product(
     id: int,
     product: schema.ProductCreate,
